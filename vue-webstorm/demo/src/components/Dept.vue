1f79f9d691eb170f3f65f7c436602da661590011
fly-1-1 <2673024411@qq.com>
2024-04-19 06:07:26 +0800

    vue-cli

---
+++ vue-webstorm/demo/src/components/Dept.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data(){
+    return{
+      msg:"部门管理"
+    }
+  }
+
+}
+
+</script>
+
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-webstorm/demo/src/components/User.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+    <table>
+      <tr>
+        <th>编号</th>
+        <th>姓名</th>
+        <th>年龄</th>
+        <th>工资</th>
+        <th>操作</th>
+      </tr>
+    </table>
+  </div>
+
+
+
+</template>
+
+<script>
+
+export default{
+  name: 'User',
+  data(){
+    return{
+      msg:'用户管理'
+    }
+  },
+
+}
+
+
+</script>
+
+<style >
+
+</style>
