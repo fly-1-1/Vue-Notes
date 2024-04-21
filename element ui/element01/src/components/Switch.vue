5e49bc299fb89dabf9be998131639408f8840e06
fly-1-1 <2673024411@qq.com>
2024-04-22 02:42:49 +0800

    switch组件

---
+++ element ui/element01/src/components/Switch.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+  <div>
+    <el-switch ref="sw" @change="aa" v-model="value" active-color="#13ce66" inactive-color="#F56C6C" :width="60" active-text="打开" inactive-text="关闭"></el-switch>
+    <el-button @click="bb">方法</el-button>
+  </div>
+</template>
+
+<script >
+  export default {
+    name: 'Switch',
+    data(){
+      return{
+        value: false
+      }
+    },
+    methods: {
+      aa(value){
+        console.log(value)
+      },
+      bb(){
+        this.$refs.sw.focus()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
