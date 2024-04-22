b3ec442f0669c1b9778c8b14714291cdc89ce092
fly-1-1 <2673024411@qq.com>
2024-04-23 05:42:02 +0800

    form组件 自定义校验规则

---
+++ element ui/element01/src/components/Form2.vue
@@ -0,0 +1,47 @@
+
+
+<template>
+  <div>
+   <el-form hide-required-asterisk :model="rform" label-width="80px" label-position="right" :rules="rules" ref="rform">
+     <el-form-item label="Name" prop="name">
+       <el-input v-model="rform.name"></el-input>
+     </el-form-item>
+     <el-button type="primary" @click="submitForm('rform')"> Submit </el-button>
+   </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      rform:{
+        name:"",
+      },
+      rules:{
+        name:[
+          { required: true, message: 'Please enter your name', trigger: 'blur' },
+          { min: 3, max: 5, message: 'Name length should be between 3 and 5', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods:{
+    submitForm(formName){
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit success');
+        } else {
+          console.log('error submit form');
+          return false;
+        }
+      })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ element ui/element01/src/components/Form3.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+  <div>
+    <el-form hide-required-asterisk :model="rform2" :rules="rules" ref="rform2">
+      <el-form-item label="Username" prop="username">
+        <el-input v-model="rform2.username"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "Form3",
+  data(){
+    let validatePass = (rule, value, callback) => {
+      if (value === "123") {
+        callback(new Error('Please input your password!'));
+      }
+    };
+
+    return{
+      rform2: {
+        username: ''
+      },
+      rules: {
+        username: [
+          { validator: validatePass, trigger: 'blur' }
+        ]
+      },
+    }
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
